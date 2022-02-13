7e0c955deb0a1f2dac89e95853277e6c65df9979
CPK <CPKbaidu@163.com>
2022-02-13 11:35:27 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ var submitDate = [
     {date: new Date(2022,  1,  3), bgColor: "#0201AC", fontColor: "#B120E7", index: 2, name: "Purple1207"},
     {date: new Date(2022,  1,  8), bgColor: "#010101", fontColor: "#0C01FF", index: 3, name: "Kieo HIN"},
     {date: new Date(2022,  1, 10), bgColor: "#FEFEFF", fontColor: "#8BD2DE", index: 4, name: "团团"},
-    {date: new Date(2022, 11, 31), bgColor: "#E8E160", fontColor: "#96893B", index: 10, name: "A"},
+    {date: new Date(2022,  1, 11), bgColor: "#E8E160", fontColor: "#96893B", index: 5, name: "A"},
+    {date: new Date(2022, 11, 31), bgColor: "#B0AD8C", fontColor: "#A8C6EE", index: 6, name: "Luojam"}
 ]
 
 /*
